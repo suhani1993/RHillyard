bb97dfb6a80f9fc72192b7bdd625edd0cb9c01ad
SuhaniShah <37039961+suhani1993@users.noreply.github.com>
2020-10-12 21:37:53 -0400

    Union-Find alternatives

---
+++ WQU_Depth.java
@@ -0,0 +1,242 @@
+/**
+ * Original code:
+ * Copyright © 2000–2017, Robert Sedgewick and Kevin Wayne.
+ * <p>
+ * Modifications:
+ * Copyright (c) 2017. Phasmid Software
+ */
+package edu.neu.coe.info6205.union_find;
+
+import java.util.Arrays;
+
+/**
+ * Height-weighted Quick Union with Path Compression
+ */
+public class WQU_Depth implements UF {
+    /**
+     * Ensure that site p is connected to site q,
+     *
+     * @param p the integer representing one site
+     * @param q the integer representing the other site
+     */
+    public void connect(int p, int q) {
+        if (!isConnected(p, q)) union(p, q);
+    }
+
+    /**
+     * Initializes an empty union–find data structure with {@code n} sites
+     * {@code 0} through {@code n-1}. Each site is initially in its own
+     * component.
+     *
+     * @param n               the number of sites
+     * @param pathCompression whether to use path compression
+     * @throws IllegalArgumentException if {@code n < 0}
+     */
+    public WQU_Depth(int n, boolean pathCompression) {
+        count = n;
+        parent = new int[n];
+        height = new int[n];
+        depth = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            height[i] = 1;
+            depth[i] = 0;
+        }
+        this.pathCompression = pathCompression;
+    }
+
+    /**
+     * Initializes an empty union–find data structure with {@code n} sites
+     * {@code 0} through {@code n-1}. Each site is initially in its own
+     * component.
+     * This data structure uses path compression
+     *
+     * @param n the number of sites
+     * @throws IllegalArgumentException if {@code n < 0}
+     */
+    public WQU_Depth(int n) {
+        this(n, true);
+    }
+
+    public void show() {
+        for (int i = 0; i < parent.length; i++) {
+            System.out.printf("%d: %d, %d %d\n", i, parent[i], height[i], depth[i]);
+        }
+    }
+
+    /**
+     * Returns the number of components.
+     *
+     * @return the number of components (between {@code 1} and {@code n})
+     */
+    public int components() {
+        return count;
+    }
+
+    /**
+     * Returns the component identifier for the component containing site {@code p}.
+     *
+     * @param p the integer representing one site
+     * @return the component identifier for the component containing site {@code p}
+     * @throws IllegalArgumentException unless {@code 0 <= p < n}
+     */
+    public int find(int p) {
+        validate(p);
+        // TO BE IMPLEMENTED
+        if(p != parent[p])
+        {
+        	p = doPathCompression(p);
+        }
+        return p;
+    }
+
+    /**
+     * Returns true if the the two sites are in the same component.
+     *
+     * @param p the integer representing one site
+     * @param q the integer representing the other site
+     * @return {@code true} if the two sites {@code p} and {@code q} are in the same component;
+     * {@code false} otherwise
+     * @throws IllegalArgumentException unless
+     *                                  both {@code 0 <= p < n} and {@code 0 <= q < n}
+     */
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    /**
+     * Merges the component containing site {@code p} with the
+     * the component containing site {@code q}.
+     *
+     * @param p the integer representing one site
+     * @param q the integer representing the other site
+     * @throws IllegalArgumentException unless
+     *                                  both {@code 0 <= p < n} and {@code 0 <= q < n}
+     */
+    public void union(int p, int q) {
+        // CONSIDER can we avoid doing find again?
+    	//We can not avoid find again because we need to assign 
+    	//parent(root) of one component as parent of other.
+    	int parentOfP = getParent(p);
+    	int parentOfQ = getParent(q);
+        mergeComponents(parentOfP, parentOfQ);
+//        if(updateHeight && (parentOfQ != q)) {
+//        	updateDepth(q, 1);
+//        }
+        count--;
+    }
+
+    @Override
+    public int size() {
+        return parent.length;
+    }
+
+    /**
+     * Used only by testing code
+     *
+     * @param pathCompression true if you want path compression
+     */
+    public void setPathCompression(boolean pathCompression) {
+        this.pathCompression = pathCompression;
+    }
+
+    @Override
+    public String toString() {
+        return "UF_HWQUPC:" + "\n  count: " + count +
+                "\n  path compression? " + pathCompression +
+                "\n  parents: " + Arrays.toString(parent) +
+                "\n  heights: " + Arrays.toString(height) +
+                "\n depth: " + Arrays.toString(depth);
+    }
+
+    // validate that p is a valid index
+    private void validate(int p) {
+        int n = parent.length;
+        if (p < 0 || p >= n) {
+            throw new IllegalArgumentException("index " + p + " is not between 0 and " + (n - 1));
+        }
+    }
+
+    private void updateParent(int p, int x) {
+        parent[p] = x;
+    }
+
+    private void updateHeight(int p, int x) {
+        height[p] += height[x];
+    }
+    
+    private void updateDepth(int p, int x) {
+        depth[p] += x;
+    }
+
+    /**
+     * Used only by testing code
+     *
+     * @param i the component
+     * @return the parent of the component
+     */
+    private int getParent(int i) {
+        return parent[i];
+    }
+
+    private final int[] parent;   // parent[i] = parent of i
+    private final int[] height;   // height[i] = height of subtree rooted at i
+    private int count;  // number of components
+    private boolean pathCompression;
+    private static int noOfConnections;
+    private final int[] depth;
+    
+    private void mergeComponents(int i, int j) {
+        // TO BE IMPLEMENTED make shorter root point to taller one
+//         if (i == j) updateParent(i, j);
+
+         // make smaller root point to larger one
+         if (height[i] < height[j]) {
+        	 updateParent(i, j);
+        	 updateHeight(j, i);
+        	 updateDepth(i, 1);
+         }else {
+        	 updateParent(j, i);
+        	 updateHeight(i, j);
+        	 updateDepth(j, 1);
+         }
+         noOfConnections++;
+//         return true;
+    }
+
+    /**
+     * This implements the single-pass path-halving mechanism of path compression
+     */
+    private int doPathCompression(int i) {
+        // TO BE IMPLEMENTED update parent to value of grandparent
+    	while (i != parent[i]) {
+            // Path compression
+			parent[i] = parent[parent[i]];
+            // find next node
+			i = parent[i];
+		}
+    	return i;
+    }
+    
+ 
+    public static void main(String[] args) {
+//    	if(args.length > 0) {
+//    		int noofSites = Integer.parseInt(args[0]);
+    		WQU_Depth data = new WQU_Depth(10);
+//		for(int i=0;i<args.length;i++) {
+//			int p = Integer.parseInt(args[++i]);
+//			int q = Integer.parseInt(args[i]);
+//			data.connect(p, q);
+//		}
+	    	data.connect(2, 4);
+			data.connect(3, 6);
+			data.connect(1, 5);
+			data.connect(7, 8);
+			data.connect(4, 6);
+			data.connect(5, 0);
+			data.connect(7, 3);
+			System.out.println("data :: " + data.toString());
+			System.out.println("No of connections :: " + noOfConnections);
+//    	}
+    }
+}
